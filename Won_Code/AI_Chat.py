da622a7f1434b3a88142cb3a3b415429bbcb0037
Won <ewon1225@gmail.com>
2026-03-10 23:33:18 -0600

    API key testing

---
+++ Won_Code/AI_Chat.py
@@ -0,0 +1,22 @@
+from apikey import APIKEY
+from openai import OpenAI
+client = OpenAI(api_key=APIKEY)
+
+conversation = []
+
+print("this is AI Chat (type 'E' to quit)")
+
+while True:
+    user_input = input("Type here: ")
+
+    if user_input.upper() == "E":
+        break
+
+    conversation.append({"role": "user", "content": user_input})
+
+    response = client.responses.create(model="gpt-5-mini", input=conversation)
+
+    reply = response.output_text
+    print("AI:", reply)
+
+    conversation.append({"role": "assistant", "content": reply})
